146052d84463df9bf1e3638fe87bf312bdf0f947
Jimmy A <metiscoda@gmail.com>
2022-10-13 22:32:14 -0700

    Initial commit

---
+++ apiKey.js
@@ -0,0 +1,12 @@
+/* Copy this into a file named apiKey.js */
+const apiKey = '1lpisFovnPFItnI8EcxIe6m97Gy7vjo3vyCA40bmG';
+const appId = 'com.metiscoda.microverse.kawadi';
+export default {apiKey, appId};
+
+// For development on local computer and local network, use a development key
+// from croquet.io/keys and create a file called apiKey-dev.js.
+
+// For deployment to a publicly accessible server, create a production key
+// from croquet.io/keys and create a file called apiKey.js.
+
+// you may export other Croquet session parameters to override default values.
